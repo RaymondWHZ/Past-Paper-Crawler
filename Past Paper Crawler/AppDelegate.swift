c8b2aed87b7ce72217a69971280ad5a51f564bee
Haozhen Wu <wuhaozhen1106@gmail.com>
2018-08-26 11:23:30 +0800

    Initial Commit

---
+++ Past Paper Crawler/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Past Paper Crawler
+//
+//  Created by 吴浩榛 on 2018/8/26.
+//  Copyright © 2018年 吴浩榛. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Past Paper Crawler/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Past Paper Crawler
+//
+//  Created by 吴浩榛 on 2018/8/26.
+//  Copyright © 2018年 吴浩榛. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
